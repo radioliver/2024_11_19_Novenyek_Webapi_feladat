019a6af8d1e9b36c74d9e04952e8f66191f163fe
radioliver <radi.oliver@diak.szbi-pg.hu>
2024-11-19 08:39:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+app.route('/', methods=["GET"])
+def index():
+    return render_template('index.html')
+
+app.route('/tabla', methods=["GET"])
+def tabla():
+    return render_template('tabla.html')
 
 plants = [
     {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
     {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
     {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
 ]
+
 
